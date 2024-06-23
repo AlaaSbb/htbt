a828e1d52647ad74b2f1c002c8e555c8b0e6b68a
Alaa Sbikhi <99049702+AlaaSbb@users.noreply.github.com>
2024-06-23 06:10:14 +0100

    Update searchV2.js

--- searchV2.js
+++ searchV2.js
@@ -1,106 +1,150 @@
-<script>
-  var queryString = window.location.search;
-  var urlParams = new URLSearchParams(queryString);
+ <script>
+    // Capture URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
 
-  $("[data-input]").each(function (index) {
-    index++;
-    var paramFlag = $(`[data-input="search-${index}"]`).data('url');
-    $(`[data-search="cms-item-${index}"]`).hide();
-    $(`[data-div="noResult-${index}"]`).hide();
-
-    var $input = $(`[data-input="search-${index}"]`);
-    var $searchItems = $(`[data-search="cms-item-${index}"]`);
-    var currentIndex = -1;
-    var $form = $input.closest('form');
-
-    $input.on("input", function () {
-      var searchVal = $(this).val().toLowerCase();
-      currentIndex = -1;
+    // Initialize search functionality for each data-input element
+    $(`[data-type="form"]`).each(function () {
+      const $input = $(this).find(`[search="input"]`);
+      const $searchItems = $(this).find(`[search="cms-item"]`);
+      const $noResultDiv = $(this).find(`[search="noResults"]`);
+      const $resultsWrapper = $(this).find(`[search="results"]`);
+      const paramFlag = $input.data('url');
+      let currentIndex = -1;
+      
+      // Hide search items, no result div, and results-wrapper initially
       $searchItems.hide();
+      $noResultDiv.hide();
+      $resultsWrapper.hide();
 
-      for (let item of $(`[data-text="search-${index}"]`)) {
-        if ($(item).text().toLowerCase().includes(searchVal) && searchVal !== "") {
-          $(`[data-text="search-${index}"]:contains("${$(item).text()}")`)
-            .parents(`[data-search="cms-item-${index}"]`)
-            .show();
+      // Clear search input functionality
+      $(document).on('click', '[data-input="clear"]', function () {
+        clearSearchInput($input);
+        $(this).hide(); // Hide the clear button after clearing the input
+      });
+
+      // Show or hide the clear button based on input value
+      $input.on('input', function () {
+        const $clearButton = $(this).closest(`[data-type="form"]`).find(`[data-input="clear"]`);
+        if ($(this).val().length > 0) {
+          $clearButton.show();
         } else {
-          $(`[data-text="search-${index}"]:contains("${$(item).text()}")`)
-            .parents(`[data-search="cms-item-${index}"]`)
-            .hide();
+          $clearButton.hide();
         }
-      }
+      });
 
-      if ($(`[data-search="cms-item-${index}"][style="display: none;"]`).length === $(`[data-search="cms-item-${index}"]`).length && searchVal !== "") {
-        $(`[data-div="noResult-${index}"]`).show();
-      } else {
-        $(`[data-div="noResult-${index}"]`).hide();
-      }
+      // Focus and input event bindings
+      $input.on("focus input", function () {
+        updateSearchResults($input, $searchItems, $noResultDiv);
+      });
 
-      if (paramFlag) {
-        var url = new URL(window.location);
-        url.searchParams.set(`search-${index}`, searchVal);
-        if (searchVal === '') {
-          url.searchParams.delete(`search-${index}`);
-        }
-        window.history.pushState({}, '', url);
-      }
-    });
+      // Keydown event binding
+      $input.on("keydown", function (e) {
+        handleKeyDown(e, $input, $searchItems);
+      });
 
-    $input.on("keydown", function (e) {
-      var visibleItems = $searchItems.filter(':visible');
+      // Search item click events
+      $searchItems.on("mousedown mouseup", function () {
+        handleSearchItemClick($input, $(this));
+      });
 
-      if (e.key === "ArrowDown") {
-        if (currentIndex < visibleItems.length - 1) {
-          currentIndex++;
-        }
-        updateSelection(visibleItems);
-        e.preventDefault();
-      } else if (e.key === "ArrowUp") {
-        if (currentIndex > 0) {
-          currentIndex--;
-        }
-        updateSelection(visibleItems);
-        e.preventDefault();
-      } else if (e.key === "Enter") {
-        if (currentIndex >= 0) {
-          var selectedText = $(visibleItems[currentIndex]).find(`[data-text="search-${index}"]`).text();
-          $input.val(selectedText).trigger('input');
-          $form.trigger('submit'); // Submit the form on Enter key
-        }
-      }
-    });
+      // Click outside handler
+      $(document).on('click', function (e) {
+        handleOutsideClick(e, $input, $searchItems, $noResultDiv);
+      });
 
-    $searchItems.on("mousedown", function () {
-      var selectedText = $(this).find(`[data-text="search-${index}"]`).text();
-      $input.val(selectedText);
-    });
-
-    $searchItems.on("mouseup", function () {
-      $input.trigger('input');
-      $form.trigger('submit'); // Submit the form on click
-    });
-
-    function updateSelection(visibleItems) {
-      visibleItems.removeClass('selected');
-      if (currentIndex >= 0) {
-        $(visibleItems[currentIndex]).addClass('selected');
-      }
-    }
-
-    if (paramFlag) {
-      if (urlParams.get(`search-${index}`)) {
-        $input.val(urlParams.get(`search-${index}`));
+      // Initialize search input with URL parameters if available
+      if (paramFlag && urlParams.get(`search`)) {
+        $input.val(urlParams.get(`search`));
         $input.trigger('input');
       }
-    }
-  });
 
-  $("<style>")
-    .prop("type", "text/css")
-    .html("\
-      .selected {\
-        background-color: #3f3e3a;\
-      }\
-    ")
-    .appendTo("head");
+      // Function to update search results based on input value
+      function updateSearchResults($input, $searchItems, $noResultDiv) {
+        const searchVal = $input.val().toLowerCase();
+        currentIndex = -1;
+        $searchItems.hide();
+        $resultsWrapper.hide();
+
+        $searchItems.each(function () {
+          const $item = $(this).find(`[data-text="search"]`);
+          if ($item.text().toLowerCase().includes(searchVal) && searchVal !== "") {
+            $(this).show();
+            $resultsWrapper.show();
+            highlightText($item, searchVal);
+          } else {
+            removeHighlight($item);
+          }
+        });
+
+        if ($searchItems.filter(':visible').length === 0 && searchVal !== "") {
+          $noResultDiv.show();
+        } else {
+          $noResultDiv.hide();
+        }
+      }
+
+      // Function to highlight matching text
+      function highlightText($item, searchVal) {
+        const text = $item.text();
+        const regex = new RegExp(`(${searchVal})`, 'gi');
+        const highlightedText = text.replace(regex, "<span class='highlight'>$1</span>");
+        $item.html(highlightedText);
+      }
+
+      // Function to remove highlight from text
+      function removeHighlight($item) {
+        $item.html($item.text());
+      }
+
+      // Function to update the visual selection in search results
+      function updateSelection(visibleItems) {
+        visibleItems.removeClass('selected');
+        if (currentIndex >= 0) {
+          $(visibleItems[currentIndex]).addClass('selected');
+        }
+      }
+
+      // Function to clear search input
+      function clearSearchInput($input) {
+        $input.val('').trigger('input');
+      }
+
+      // Function to handle keydown events
+      function handleKeyDown(e, $input, $searchItems) {
+        const visibleItems = $searchItems.filter(':visible');
+        if (e.key === "ArrowDown") {
+          if (currentIndex < visibleItems.length - 1) {
+            currentIndex++;
+          }
+          updateSelection(visibleItems);
+          e.preventDefault();
+        } else if (e.key === "ArrowUp") {
+          if (currentIndex > 0) {
+            currentIndex--;
+          }
+          updateSelection(visibleItems);
+          e.preventDefault();
+        } else if (e.key === "Enter" && currentIndex >= 0) {
+          const selectedText = $(visibleItems[currentIndex]).find(`[data-text="search"]`).text();
+          $input.val(selectedText).trigger('input');
+          $input.closest('form').trigger('submit'); // Submit the form on Enter key
+        }
+      }
+
+      // Function to handle search item click events
+      function handleSearchItemClick($input, $item) {
+        const selectedText = $item.find(`[data-text="search"]`).text();
+        $input.val(selectedText).trigger('input');
+        $input.closest('form').trigger('submit'); // Submit the form on click
+      }
+
+      // Function to handle clicks outside the search input
+      function handleOutsideClick(e, $input, $searchItems, $noResultDiv) {
+        if (!$input.is(e.target) && $input.has(e.target).length === 0) {
+          $searchItems.hide();
+          $noResultDiv.hide();
+          $resultsWrapper.hide();
+        }
+      }
+    });
 </script>
